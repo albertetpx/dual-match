3cf953225be538570e4304bf5f8db7d3163968d3
albertetpx <aguardiola@etpx.fjaverianas.com>
2025-06-28 15:39:40 +0200

    h-screen en layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body className="min-h-screen flex flex-col">
+      <body className="h-screen flex flex-col">
         <Header />
         <main className="flex-1">{children}</main>
         <Footer />
